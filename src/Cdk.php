dde1e990364576c56fe30e8f58fd2ccb737e09b3
Joseph Alonzi <info@alonzi.com>
2022-11-14 10:59:35 +0300

    Extract Data + Parse Decoupling

--- src/Cdk.php
+++ src/Cdk.php
@@ -6,11 +6,14 @@ include_once __DIR__ . '/Core/Core.php';
 class Cdk extends Core {
 
     public function setConfig(){
-        
+
         $this->config = new CdkConfig(
             $this->authentication,
             $this->environment,
-            $this->testSuiteConfig
+            $this->testSuiteConfig,
+            $this->cache,
+            $this->cacheDir,
+            $this->rawDir
         );
 
         $this->tests->setTestSuiteConfig(
